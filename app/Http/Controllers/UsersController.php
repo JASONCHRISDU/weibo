e7aedfa177eb80eca2d5ca187351051378757d89
Jason <jasonchrisdudu@gmail.com>
2019-02-08 11:03:18 +0800

    [190208: add laravel-lang]

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 use App\Models\User;
 
@@ -16,5 +17,15 @@ class UsersController extends Controller
     {
         return view('users.show', compact('user'));
     }    
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+        return;
+    }
 }
 
